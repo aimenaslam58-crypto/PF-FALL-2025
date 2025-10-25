ba6444c9c85a7cdc7435af7897dca87306b8d10c
aimenaslam58-crypto <aimenaslam58@gmail.com>
2025-10-25 10:46:57 -0700

    s

--- Lab manual/lab 2/lab 2 Q 1.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("\a\a\a\a\a");
-    return 0;
-}
--- Lab manual/lab 2/lab 2 Q 3.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("*\n**\n***\n****");
-    printf("\n\n");
-    printf("*\n*\t*\n*\t*\t*\n*\t*\n*");
-    return 0;
-}
